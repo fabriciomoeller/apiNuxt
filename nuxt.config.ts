9ff8cdd194f251c764572ebf945f42f56bbdddcf
fabriciomoeller <fabriciomoeller@gmail.com>
2023-11-18 19:46:44 -0300

    No Middleware Auth foi adiciona o cache para o token.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  $production: {
-    nitro: {
-      storage: {
-        cache: {
-          driver: 'redis',
-        }
-      }
-    }  
-  },
   nitro: {
     storage: {
-      cache: {
-        driver: 'memory',
+      user: {
+        driver: 'fs',
+        base: '.cache'
       }
     }
 
--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -2,7 +2,17 @@ export default defineEventHandler(async event => {
     const token = getCookie(event, 'token'); 
     event.context.user = { 
         id: 'hi'
-    }  
+    } 
+
+    const storage = useStorage('user');
+    if (await storage.hasItem('token')) {
+        await storage.setItem('token', 'update token');
+    } else {
+        await storage.setItem('token', 'new token');    
+    }    
+    
+    //await useStorage().setItem('user:token', 'hi');
+
     if (event.path.startsWith('/api/user') && token !== 'hi') {      
         throw createError({
             status: 401,
