11984485284ec4c8cceb22317daa6e003f40180d
fabriciomoeller <fabriciomoeller@gmail.com>
2023-11-20 21:58:25 -0300

    Adicionando cache para a sessao

--- server/api/user/[id].ts
+++ server/api/user/[id].ts
@@ -1,8 +1,9 @@
-export default defineEventHandler(async event => {
+export default defineCachedEventHandler(async event => {
     const id = getRouterParam(event, 'id');
     
+    await new Promise(resolve => setTimeout(resolve, 5000));
     return {   
         id,
         session: event.context.user.id,
     }
-})
+}, {swr: true })
